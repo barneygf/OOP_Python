f701dc92aa24e033772cae8cabadb521026aaa4a
Maciek <maciej.michal.barnas@gmail.com>
2017-10-11 13:50:29 +0200

    more methods in second file

--- 2_point_class.py
+++ 2_point_class.py
@@ -1,8 +1,23 @@
-class Point:
-    def reset(self):
-        self.x = 0
-        self.y = 0
+import math
 
-p = Point()
-p.reset()
-print(p.x, p.y)
+class Point:
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    def reset(self):
+        self.move(0, 0)
+
+    def calculate_distance(self, other_point):
+        return math.sqrt((self.x - other_point.x)**2 + (self.y - other_point.y)**2)
+
+point1 = Point()
+point2 = Point()
+
+point1.reset()
+point2.move(4, 8)
+print(point2.calculate_distance(point1))
+assert (point1.calculate_distance(point2) == point2.calculate_distance(point1))  # Debugging
+point1.move(2,11)
+print(point1.calculate_distance(point2))
+print(point2.calculate_distance(point2))
