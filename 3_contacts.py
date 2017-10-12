2d2089a2dffa0ba943c3e576638f78565ef8e0db
Maciek <maciej.michal.barnas@gmail.com>
2017-10-12 13:53:56 +0200

    changing methods in existing class in child class

--- 3_contacts.py
+++ 3_contacts.py
@@ -19,13 +19,22 @@ class Supplier(Contact):
     def order(self, order):
         print("If this were a real system we would send " "'{}' order to '{}'".format(order, self.name))
 
+class Friend(Contact):
+    def __init__(self, name, email, phone):
+        'Modificate __init__ method of the Contact class'
+        super().__init__(name, email)
+        self.phone = phone
+
 c1 = Contact("John A", "johna@example.net")
 c2 = Contact("John B", "johnb@example.net")
 c3 = Contact("Jenna C", "jennac@example.net")
 s = Supplier("Sup Plier", "supplier@example.net")
+f1 = Friend("George", "george@example.net", '874-852-4105')
 
 print(c1.name, c1.email, s.name, s.email)
 print('====')
 print(s.order("I need pliers"))
 print('====')
 print([c.name for c in Contact.all_contacts.search('John')])
+print('====')
+print(f1.phone)
