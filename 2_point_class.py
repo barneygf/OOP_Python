b8da2ba7493f35d8060f4a37c5fe57a60004e70a
Maciek <maciej.michal.barnas@gmail.com>
2017-10-12 11:00:37 +0200

    new file - inheritance

--- 2_point_class.py
+++ 2_point_class.py
@@ -1,14 +1,29 @@
 import math
 
 class Point:
+    'Represents a point in two-dimensional geometric coordinates'
+
+    def __init__(self, x = 0, y = 0):
+        '''Initialize the position of a new point. The x and y
+        coordinates can be specified. If they are not, the
+        point defaults to the origin.'''
+        self.move(x, y)
+
     def move(self, x, y):
+        "Move the point to a new location in 2D space."
         self.x = x
         self.y = y
 
     def reset(self):
+        'Reset the point back to the geometric origin: 0, 0'
         self.move(0, 0)
 
     def calculate_distance(self, other_point):
+        """Calculate the distance from this point to a second
+        point passed as a parameter.
+        This function uses the Pythagorean Theorem to calculate
+        the distance between the two points. The distance is
+        returned as a float."""
         return math.sqrt((self.x - other_point.x)**2 + (self.y - other_point.y)**2)
 
 point1 = Point()
@@ -21,3 +36,4 @@ assert (point1.calculate_distance(point2) == point2.calculate_distance(point1)) 
 point1.move(2,11)
 print(point1.calculate_distance(point2))
 print(point2.calculate_distance(point2))
+print(help(Point))
